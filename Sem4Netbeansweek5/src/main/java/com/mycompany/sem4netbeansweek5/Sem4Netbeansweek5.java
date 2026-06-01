9189624615bdd816ec4e3e0df7425911b3e3920c
Owner <Owner@10.48.17.153>
2026-06-01 12:17:56 -0400

    helloworld

---
+++ Sem4Netbeansweek5/src/main/java/com/mycompany/sem4netbeansweek5/Sem4Netbeansweek5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sem4netbeansweek5;
+
+/**
+ *
+ * @author Owner
+ */
+public class Sem4Netbeansweek5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
